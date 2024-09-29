79eea8b572edd9a065afc3c4a33703795899ccb0
wuerion <almerux@gmail.com>
2024-09-29 17:26:38 -0600

    creacion de la logica para obtener los gradientes de la db

---
+++ get_gradients.php
@@ -0,0 +1,43 @@
+<?php
+// Permitir acceso desde http://localhost:4321
+header("Access-Control-Allow-Origin: http://localhost:4321");
+// Permitir los métodos que se van a utilizar, como GET o POST
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// Permitir encabezados adicionales si es necesario
+header("Access-Control-Allow-Headers: Content-Type");
+
+header('Content-Type: application/json');
+
+//conectar a la db
+include 'db_connection.php';
+
+try {
+    $query = $conn->prepare("SELECT * FROM gradients");
+    $query->execute();
+    $colors = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    // verificar si hay resultados
+    if ($colors) {
+        // devolver si hay resultados
+        echo json_encode([
+            'status' => 'success',
+            'data' => $colors
+        ]);
+    } else {
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'No se encotraron datos'
+        ]);
+    }
+} catch (PDOException $e) {
+    // manejo de errores de la db
+    echo json_encode([
+        'status' => 'error',
+        'message'=> 'Error en la base de datos: ' . $e->getMessage()
+    ]);
+}
+
+if (!$conn) {
+    echo "no conectado";
+    exit();
+}
